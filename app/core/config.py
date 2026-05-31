62f0030cb61186122063830bc7af34e601a8bb6b
nicholas-saw <67905907+nicholas-saw@users.noreply.github.com>
2026-05-31 17:56:11 +0800

    change db

--- app/core/config.py
+++ app/core/config.py
@@ -3,4 +3,4 @@ import os
 # Directs to Lumino-1 folder
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-DATABASE_URL = f"sqlite+aiosqlite:///{BASE_DIR}/database/mice.db"
+DATABASE_URL = f"sqlite+aiosqlite:///{BASE_DIR}/database/mice3.db"
