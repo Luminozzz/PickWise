d12670236cff79b84af689b8fec98a78c1980666
Shir0-Kage <zhouzehao1783174@gmail.com>
2026-06-01 01:58:20 +0800

    frontend

--- app/core/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Directs to Lumino-1 folder
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-DATABASE_URL = f"sqlite+aiosqlite:///{BASE_DIR}/database/mice3.db"
